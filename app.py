670738d2506f52abf891ea19e816ed3c5eea4d9c
unknown <79578683+Treszyk@users.noreply.github.com>
2024-03-13 20:15:13 +0100

    Extracting opinion and product details

--- app.py
+++ app.py
@@ -17,11 +17,43 @@ def extract(id='', error=''):
     print(id)
     if id == '':
         return '<h1>extract_menu</h1>'
+    elif id in products.keys():
+        print('bez dodatkowego pobierania')
+        return render_template('extract.html', opinions = products[id].opinions, product = products[id])
     else:
-        #opinions = scraper.scrape_opinions('145964652')
-        #print(opinions)
-        return f'<h1>extracting {id}... and then redirect to product page if the id is valid else redirect to extract with an error </h1>'
-    
+        try:
+            scraped_data = scraper.scrape_opinions(id)
+        except Exception as error:
+            print(error)
+            return '<h1>error</h1>'
+        opinions_html = scraped_data[0]
+        product_info = scraped_data[1]
+        opinions = []
+
+        new_product = Product(id, product_info['name'], product_info['img'], product_info['num_reviews'])
+
+        print(opinions_html)
+        for opinion in opinions_html:
+            extracted_details = scraper.extract_details(opinion)
+            opinion = Opinion(extracted_details)
+            opinions.append(opinion)
+        
+        new_product.set_opinions(opinions)
+        
+        products[id] = new_product
+
+        return render_template('extract.html', opinions = new_product.opinions, product = new_product)
+
+@app.route('/product/')
+@app.route('/product/<id>')
+def product_page(id):
+    if id == '':
+        return '<h1>Prosze wprowadzic prawidlowy kod produktu</h1>'
+    elif id != '' and id not in products.keys():
+        return '<h1>Prosze najpierw pobrac opinie tego produktu</h1>'
+    else:
+        return '<h1>amogu</h1>'
+        
 if __name__ == '__main__':
     app.run()
 
--- opinion.py
+++ opinion.py
@@ -1,5 +1,17 @@
 class Opinion:
-    def __init__(self, id):
-        self.id = id
-    def load_details(extracted_details) -> None:
-        pass
+    def __init__(self, extracted_details):
+        self.load_details(extracted_details)
+    def load_details(self, extracted_details) -> None:
+        #id:str, author:str, recommended:bool, score:int, verified:bool, review_date:str, buy_date:str, likes:int, dislikes:int, content:str, plus:int, minus:int
+        self.id = extracted_details['id']
+        self.author = extracted_details['author']
+        self.recommended = extracted_details['recommended']
+        self.score = extracted_details['score']
+        self.verified = extracted_details['verified']
+        self.review_date = extracted_details['review_date']
+        self.buy_date = extracted_details['buy_date']
+        self.likes = extracted_details['likes']
+        self.dislikes = extracted_details['dislikes']
+        self.content = extracted_details['content']
+        self.plus = extracted_details['plus']
+        self.minus = extracted_details['minus']
