42a21041fb0253fe633dc8f95c4da8a7624266da
unknown <79578683+Treszyk@users.noreply.github.com>
2024-03-14 14:24:06 +0100

    Added extraction menu

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for, render_template
 from scraper import Scraper
 from opinion import Opinion
 from product import Product
+from flask import request
 
 app = Flask(__name__)
 products = {}
@@ -11,21 +12,25 @@ scraper = Scraper()
 def home():
     return render_template('home.html')
 
-@app.route('/extract/')
-@app.route('/extract/<id>')
+@app.route('/extract/', methods=['GET'])
 def extract(id='', error=''):
+    id = request.args.get('id', '')
+    error = request.args.get('error', '')
+    submitted = True if request.args.get('form_submitted') == 'true' else False
     print(id)
     if id == '':
-        return '<h1>extract_menu</h1>'
+        if submitted:
+            error = 'Prosze wpisac poprawny kod!'
+        return render_template('extract.html', error = error)
     elif id in products.keys():
         print('bez dodatkowego pobierania')
-        return render_template('extract.html', opinions = products[id].opinions, product = products[id])
+        return redirect(url_for('product_page', id=id))#('extract.html', opinions = products[id].opinions, product = products[id])
     else:
         try:
             scraped_data = scraper.scrape_opinions(id)
         except Exception as error:
             print(error)
-            return '<h1>error</h1>'
+            return redirect(url_for('extract', error='Prosze wpisac poprawny kod'))
         opinions_html = scraped_data[0]
         product_info = scraped_data[1]
         opinions = []
@@ -42,17 +47,17 @@ def extract(id='', error=''):
         
         products[id] = new_product
 
-        return render_template('extract.html', opinions = new_product.opinions, product = new_product)
+        return redirect(url_for('product_page', id=id))#('extract.html', opinions = new_product.opinions, product = new_product)
 
 @app.route('/product/')
 @app.route('/product/<id>')
 def product_page(id):
     if id == '':
-        return '<h1>Prosze wprowadzic prawidlowy kod produktu</h1>'
+        return redirect(url_for('extract'))
     elif id != '' and id not in products.keys():
-        return '<h1>Prosze najpierw pobrac opinie tego produktu</h1>'
+        return redirect(url_for('extract', error='Prosze najpierw pobrac opinie tego produktu'))#'<h1>Prosze najpierw pobrac opinie tego produktu</h1>'
     else:
-        return '<h1>amogu</h1>'
+        return render_template('product.html', opinions = products[id].opinions, product=products[id])
         
 if __name__ == '__main__':
     app.run()
